41fa9dba7816bf2d70d977f0f7c84cd75064c986
Márk <mark.varga@tmrw.life>
2023-07-23 23:26:49 +0200

    sizes entity

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,33 @@
+import prismadb from "@/lib/prismadb";
+import { BillboardClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
+import { format } from "date-fns";
+
+const SizePage = async ({ params }: { params: { storeId: string } }) => {
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedSizes: SizeColumn[] = sizes.map((size) => {
+    return {
+      id: size.id,
+      name: size.name,
+      value: size.value,
+      createdAt: format(size.createdAt, "MMMM do, yyyy"),
+    };
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedSizes} />
+      </div>
+    </div>
+  );
+};
+export default SizePage;
